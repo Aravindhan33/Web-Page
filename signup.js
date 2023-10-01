be68f2345ccea175c51c81cdf8c4583317ebb871
Aravindhan <115220948+Aravindhan33@users.noreply.github.com>
2023-10-01 22:25:33 +0530

    Add files via upload
    signup and login form code

---
+++ signup.js
@@ -0,0 +1,63 @@
+// ^ STORING DATA IN LOCAL STORAGE
+let form=document.querySelector("form")
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    let username = document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+    if(username===""||password==""){
+        alert("both should be filled")
+    }else if(username!="" && password!="" && username.length>=6 && password.length>=6){
+        localStorage.setItem("username", username)
+        localStorage.setItem("password", password)
+        confirm("signed up successfully")
+        window.open("login.html")
+    }
+})
+
+
+// ! SHOW PASSWORD
+function show_pwd(){
+    let password=document.getElementById("password")
+    if(password.type=="password")
+    password.type="text"
+else
+password.type="password"
+
+}
+
+let username=document.getElementById("username")
+let error1=document.getElementById("error1");
+let submit_button=document.getElementById("submit_button")
+username.addEventListener("keyup",()=>{
+    let validation =username.value.length<6;
+    if(validation==true){
+        username.classList.add("warning")
+        username.classList.remove("success")
+        error1.innerHTML="*username should be 6 characters*"
+        submit_button.disabled=true
+        submit_button.classList.add("disabled")
+    }else{
+        username.classList.add("success")
+        username.classList.remove("warning")
+        error1.innerHTML=""
+        submit_button.disabled=false
+        submit_button.classList.remove("disabled")
+    }
+    
+} )
+
+let password=document.getElementById("password")
+let error2=document.getElementById("error2");
+password.addEventListener("keyup",()=>{
+    let validation =password.value.length<6;
+    if(validation==true){
+        password.classList.add("warning")
+        password.classList.remove("success")
+        error2.innerHTML="*password should be 6 characters*"
+    }else{
+        password.classList.add("success")
+        password.classList.remove("warning")
+        error2.innerHTML=""
+    }
+    
+} )
